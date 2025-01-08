21047c21ac6d8df41e435393ee2cd9a43c68a273
yelyzavetanov <164746183+yelyzavetanov@users.noreply.github.com>
2025-01-08 11:55:16 +0300

    Fix bugs

--- src/components/FinancialCalculatorForm/constants.js
+++ src/components/FinancialCalculatorForm/constants.js
@@ -1,5 +1,5 @@
 export const inputFields = [
-    { id: 'car_price', label: 'Car Price ($)', placeholder: 'Enter home price' },
+    { id: 'car_price', label: 'Car Price ($)', placeholder: 'Enter car price' },
     { id: 'cash_down', label: 'Cash Down ($)', placeholder: 'Enter cash down' },
     { id: 'apr', label: 'APR (%)', placeholder: 'Enter APR' },
     { id: 'btc_price', label: 'BTC Price ($)', placeholder: 'Enter BTC price' },
