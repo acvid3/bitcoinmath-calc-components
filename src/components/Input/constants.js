e6b64872026a872619e6ee57248c7dadc3f7e670
yelyzavetanov <164746183+yelyzavetanov@users.noreply.github.com>
2025-01-27 16:47:54 +0300

    Add descriptions to form inputs

---
+++ src/components/Input/constants.js
@@ -0,0 +1,6 @@
+export const inputDescriptions = [
+    { description: "How much cash do you have to invest in this product?", label: "Total cash savings ($)" },
+    { description: "What's the Annuall Percentage Yield paid to you?", label: "APY" },
+    { description: "What's the true measure of inflation impacting your dollars each year?", label: "Inflation percent (%)" },
+    { description: "How many years do you want to use this product?", label: "Number of years" },
+];
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import { TextField, Box, Typography } from '@mui/material';
-import {containerStyles, inputStyles, labelStyles, messageStyles} from './styles';
+import {TextField, Box, Typography, Button} from '@mui/material';
 import { useResult } from '../../context/ResultContext';
+import {inputDescriptions} from "./constants";
+import { sx } from "./styles";
 
 const Input = ({ id, label, value, onChange, placeholder, message }) => {
     const { results } = useResult();
 
     const isInputError = results?.code?.includes(id);
 
+    const description = inputDescriptions.find(d => d.label === label)?.description;
+
     return (
-        <Box sx={containerStyles}>
-            {label && <Typography sx={labelStyles}>{label}</Typography>}
+        <Box sx={sx.container}>
+            {label && <Typography sx={sx.label}>
+                {label}
+                {description &&
+                    <span>
+                        <Button sx={sx.descriptionIcon}>i</Button>
+                        <Box className={'description'} sx={sx.description}>
+                            {description}
+                        </Box>
+                    </span>
+                }
+            </Typography>}
             <TextField
                 id={id}
                 value={value}
@@ -18,12 +31,11 @@ const Input = ({ id, label, value, onChange, placeholder, message }) => {
                 placeholder={placeholder}
                 variant="outlined"
                 fullWidth
-                sx={inputStyles}
+                sx={sx.input}
                 type="number"
                 error={results?.data?.status === 400 && isInputError}
                 helperText={isInputError ? results?.message?.replace(/_/g, ' ') : ''}
             />
-            {message && <Typography sx={messageStyles}>{message}</Typography>}
         </Box>
     );
 };
