72e97457584157d55c683819b33f645799ba9aa2
yelyzavetanov <164746183+yelyzavetanov@users.noreply.github.com>
2025-01-30 10:49:15 +0300

    Add animals to cagr

--- src/components/CagrInputRange/styles.js
+++ src/components/CagrInputRange/styles.js
@@ -1,7 +1,7 @@
 export const styles = {
     container: {
         width: '100%',
-        padding: '15px 20px 20px',
+        padding: '10px 35px 10px 25px',
         boxSizing: 'border-box',
         borderRadius: '30px',
         backgroundColor: '#E9EBE4',
@@ -19,34 +19,49 @@ export const styles = {
     slider: {
         color: '#ffffff',
         height: '8px',
+        margin: '10px',
 
         '& .MuiSlider-thumb': {
             width: '16px',
             height: '16px',
             backgroundColor: '#2E4E35',
+            '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                boxShadow: 'none', // Прибирає блакитний фон
+            },
+        },
+        "& .MuiSlider-mark": {
+            opacity: 0,
+            background: 'red',
+            color: 'red',
+            padding: '2px',
+            borderRadius: '50%',
         },
         '& .MuiSlider-markLabel': {
-            transform: 'translateX(-50%) translateY(10px)',
+            // transform: 'translateX(-50%) translateY(10px)',
+        },
+        "& .MuiSlider-mark[data-index='0'], .MuiSlider-mark[data-index='1'], .MuiSlider-mark[data-index='2']": {
+            background: '#F1B314',
+            opacity: 1,
+        },
+        "& .MuiSlider-markLabel[data-index='0'], .MuiSlider-markLabel[data-index='1'], .MuiSlider-markLabel[data-index='2'],": {
+            top: '-10px',
+            marginLeft: '5px',
+            // marginTop: '10px',
+            // margin: '10px 0 10px 5px',
         },
         '& .MuiSlider-track': {
             backgroundColor: '#2E4E35',
         },
         "& .MuiSlider-rail": {
             opacity: 1,
-            backgroundColor: "#fff",
-        },
-        "& .MuiSlider-mark": {
-            opacity: 0,
         },
         '& .MuiSlider-valueLabel': {
             backgroundColor: '#E9EBE4',
             color: '#F1B314',
             borderRadius: '10px',
             fontWeight: '700',
-            // fontSize: '0.875rem',
-            padding: '2px 6px',
-            // top: '-10px',
-            // transform: 'none',
+            top: '40px',
+            padding: '0px 10px',
         },
     }
 }
