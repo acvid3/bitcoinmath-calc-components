87127b37e9cd5062683525ac1f6e544bc47e20ff
yelyzavetanov <164746183+yelyzavetanov@users.noreply.github.com>
2025-01-30 10:25:53 +0300

    Fix styles

--- src/components/CagrInputRange/styles.js
+++ src/components/CagrInputRange/styles.js
@@ -1,7 +1,7 @@
 export const styles = {
     container: {
         width: '100%',
-        padding: '10px 30px 10px 25px',
+        padding: '10px 35px 10px 25px',
         boxSizing: 'border-box',
         borderRadius: '30px',
         backgroundColor: '#E9EBE4',
