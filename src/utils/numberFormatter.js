a316f968ec7ea4902e78f9baed49c12d8def56b9
yelyzavetanov <164746183+yelyzavetanov@users.noreply.github.com>
2025-02-12 14:35:35 +0300

    Fix bugs

--- src/utils/numberFormatter.js
+++ src/utils/numberFormatter.js
@@ -1,7 +1,7 @@
 export const formatNumber = (number) => {
     if (!number) return;
 
-    if (number >= 100) {
+    if (Math.abs(number) >= 100) {
         return Math.floor(number).toLocaleString('en-US');
     } else {
         return number.toFixed(2);
